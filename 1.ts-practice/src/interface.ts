c888a7d8152fccc461bb86998f922353e803dbac
SC <topsky2008@gmail.com>
2020-11-05 15:05:10 +0900

    generic

--- 1.ts-practice/src/interface.ts
+++ 1.ts-practice/src/interface.ts
@@ -34,25 +34,68 @@ const rectangle = new Rectangle(10,10);
 console.log(circle.radius);
 // console.log(rectangle.width); //error
 
-interface Person {
-    name : string;
-    age : number;
-}
+// interface Person {
+//     name : string;
+//     age : number;
+// }
 
 // interface Developer {
 //     name : string;
 //     age : number;
 //     skills : string[];
 // }
-interface Developer extends Person {
-    skills : string[];
+// interface Developer extends Person {
+//     skills : string[];
+// }
+// const person : Person = {
+//     name : "김계란",
+//     age : 30,
+// };
+// const expert: Developer ={
+//     name : "김개발",
+//     age : 28,
+//     skills : ["js","react"]
+// };
+
+type Person = {
+    name : string;
+    age ?: number;
 }
+
+type Developer = Person & {
+    skills :string[]
+}
+
 const person : Person = {
-    name : "김계란",
-    age : 30,
+    name : "김계란"
 };
-const expert: Developer ={
+
+const expert : Developer = {
     name : "김개발",
-    age : 28,
-    skills : ["js","react"]
+    skills : ["js","ts","react"],
 };
+
+type People = Person[];
+
+const people : People = [person,expert];
+
+function Wrap<T>(param : T) {
+    return {param};
+};
+
+const wrap = Wrap('abc');
+
+class Queue<T> {
+    private items : T[]=[];
+    constructor(){}
+    enqueue(item : T):void{
+        this.items.push(item);
+    }
+    dequeue() {
+        return this.items.shift();
+    }
+    get length():number{
+        return this.items.length;
+    }
+}
+
